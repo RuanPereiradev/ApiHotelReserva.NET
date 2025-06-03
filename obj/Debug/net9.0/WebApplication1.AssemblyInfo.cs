bd550b58ab797b85ce6a2f1ecab59589cffc9014
RuanPereiradev <ruanpereira@alu.ufc.br>
2025-06-03 14:52:48 -0300

    Resolve conflitos após pull

--- obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4b580766cb960b31c3168619590f9d9894b280e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c237de976cc2a2cc457f38aaa7f2e26d5cad2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
